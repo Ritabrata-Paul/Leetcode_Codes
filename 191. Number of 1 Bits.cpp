8577705e6ef879cc02a1eb87ced0c3175917ccf2
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-09-08 21:37:08 +0530

    day 2 program

---
+++ 191. Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int one=0;
+        
+        while(n>0)
+        {
+            one++;
+            n=n &(n-1);
+        }
+        return one;
+    }
+};
