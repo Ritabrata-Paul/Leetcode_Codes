c9ea7fbc64af7b1f24188cf6e46da8841f0b6ab2
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-09-15 01:06:38 +0530

    solution change

--- 393. UTF-8 Validation.cpp
+++ 393. UTF-8 Validation.cpp
@@ -15,7 +15,62 @@ public:
                 len = 3;
             }else if ( (data[i] & 0xF8) == 0xF0) { // checking 11110xxx
                 len = 4;
-            }else {
+            }else {class Solution {
+public:
+    int csb(int n){
+    int curr=7;
+    int msb=0;
+    while(curr>=0){
+        if((n&(1<<curr))>0){
+            msb++;
+        }
+        else{
+            return msb;
+        }
+        curr--;
+    }
+    return msb;
+}
+bool validUtf8(vector<int>& data) {
+    if(csb(data[0])==1){
+        return false;
+    }
+    int c=0;
+    for(int i=0;i<data.size();i++){
+        int cb=csb(data[i]);
+        if(cb>4){
+            return false;
+        }
+        if(cb==1){
+            if(c==0){
+                return false;
+            }
+            else{
+                c--;
+                if(c<0){
+                    return false;
+                }
+            }
+        }
+        else if(cb==0){
+            if(c!=0)
+            return false;
+        }
+        else{
+            if(c!=0){
+                return false;
+            }
+            else{
+                c=cb-1;
+            }
+        }
+    }
+    if(c==0){
+        return true;
+    }
+    return false;
+}
+};
                 return false;
             }
             
